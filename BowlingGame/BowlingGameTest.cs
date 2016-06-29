7363b70470d9143721ca8f16695e5fa7875a01f9
Chad Stever <chad@chadstever.com>
2016-06-29 00:35:34 -0500

    initial commit; kata complete

---
+++ BowlingGame/BowlingGameTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BowlingGame
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+
+        private Game g;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            g = new Game();
+        }
+        
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void testAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, g.Score());
+        }
+
+        [TestMethod]
+        public void testOneSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            RollMany(16, 0);
+            Assert.AreEqual(24, g.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.AreEqual(300, g.Score());
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+C# implementation of Bob Martin Bowling Game Kata
+
+http://www.butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
